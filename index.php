068b35893de68fa7640fe605d06c301bb88ca303
YungShengLu <jones830802@gmail.com>
2016-01-19 00:50:07 +0800

    20160119 12:50

--- index.php
+++ index.php
@@ -9,70 +9,204 @@
 
 <html lang="en">
 <head>
-  <title>Bookmark</title>
+  <title>Jigsawords</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/login.css">
   <link rel="stylesheet" href="css/stylesheet.css">
+
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </head>
 
-<body>
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="index.php">Bookmark</a>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav navbar-right">
-        <li><a class="navbar-link" href="register.php"><span class="glyphicon glyphicon-flag"></span> Register</a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
+<body id="page-top" class="index">
 
-<div class="container">
-  <div class="card card-container">
-      <p id="subtitle">Sign in with your Bookmark</p>
-      <img id="profile-img" class="profile-img-card"/>
-      <p id="profile-name" class="profile-name-card"></p>
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#page-top">JIGSAWORDS</a>
+            </div>
 
-      <form class="form-signin" action="login.php" method="post">
-        <span id="reauth-email" class="reauth-email"></span>
-        
-        <input name="email" type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-        <input name="password" type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-        
-        <div id="remember" class="checkbox">
-          <label>
-            <input mame="remember-me" type="checkbox" value="remember-me"> Remember me
-          </label>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="navbar-page page-scroll">
+                        <a href="#rule">Rule</a>
+                    </li>
+
+                    <li class="navbar-page page-scroll">
+                        <a href="#register">Register</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
         </div>
+        <!-- /.container-fluid -->
+    </nav>
 
-        <?php
-          if ($_SESSION['login_user'] == 'wrong') 
-            echo "<p id='wrong'>Wrong email or password!</p>";
-          else
-            echo "";
-          $_SESSION['login_user'] = '';
-        ?>
+    <div id="main">
+    <!-- Header -->
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <img class="img-responsive" src="img/game.png" alt="">
+                    <div class="intro-text">
+                        <span class="name">Word is power!</span>
+                        <div class="col-lg-8 col-lg-offset-2 text-center page-scroll">
+                            <a href="#signin" class="btn btn-lg btn-outline">
+                                <i class="fa"></i>SIGN IN
+                            </a>
+                        </div>
+                        <hr class="star-light">
+                        <span class="skills">Find out more and more words as you can!</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
 
-        <button name="submit" class="btn btn-lg btn-primary btn-block btn-signin" id="submit" type="submit">Sign in</button>
-      </form><!-- /form -->
-      <a href="#" class="forgot-password">Forgot the password?</a>
-    </div><!-- /card-container -->
-  </div><!-- /container -->
+     <!-- Sign in Section -->
+    <section class="signin" id="signin">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Sign in</h2>
+                    <hr class="star-primary">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <form name="sentMessage" id="contactForm" action="login.php" method="post">
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Email Address</label>
+                                <input name="email" type="text" class="form-control" placeholder="Email Address" id="email" required data-validation-required-message="Please enter your email address.">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Password</label>
+                                <input name="password" type="password" class="form-control" placeholder="Password" id="password" required data-validation-required-message="Please enter your password.">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
 
-<footer class="container-fluid text-center">
-    <div class='footer-copyright'>
-    <strong>Copyright &copy; David Lu</strong>
-    <br>
-    2015 National Chung Kung University Computer Science and Information Egineering.
-    <div class='footer-background'>WEB APPLICATION AND PROGRAMMING</div>
-  </div>
-</footer>
+                        <br>
+                        <div class="col-lg-8 col-lg-offset-2 text-center">
+                            <div class="form-group col-xs-12">
+                                <input name="submit" type="submit" class="btn btn-inline btn-lg" id="submit" value="Sign in">
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
 
-</body>
+    <!-- Rule Section -->
+    <section class="rule" id="rule">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>About</h2>
+                    <hr class="star-light">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-lg-offset-2">
+                    <p>GAME RULE</p>
+                </div>
+                <div class="col-lg-4">
+                    <p>GAME RULE</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Register Section -->
+    <section class="register" id="register">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Register</h2>
+                    <hr class="star-primary">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <form name="sentMessage" id="contactForm" method="post">
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Name</label>
+                                <input type="text" class="form-control" placeholder="Name" id="name" required data-validation-required-message="Please enter your name.">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Email Address</label>
+                                <input type="email" class="form-control" placeholder="Email Address" id="email" required data-validation-required-message="Please enter your email address.">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Password</label>
+                                <input type="password" class="form-control" placeholder="Password" id="password" required data-validation-required-message="Please enter your phone number.">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="row control-group">
+                            <div class="form-group col-xs-12 floating-label-form-group controls">
+                                <label>Confirm password</label>
+                                <input type="password" class="form-control" placeholder="Confirm password" id="password" required data-validation-required-message="Please enter your phone number.">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <br>
+                        <div id="register"></div>
+                        <div class="col-lg-8 col-lg-offset-2 text-center">
+                            <div class="form-group col-xs-12">
+                                <button type="submit" class="btn btn-inline btn-send btn-lg">Send</button>
+                                <button type="reset" class="btn btn-inline btn-lg">Clear</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <footer class="text-center">
+        <div class="footer-below">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">Copyright &copy; 2014</div>
+                    <div class="col-lg-13">2015 National Cheng Kung University Computer Science and Information Engineering.</div>
+                </div>
+                <div class="back">
+                    <div class="col-lg-14">WEB APPLICATION AND PROGRAMMING</div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
+    <div class="scroll-top page-scroll visible-xs visible-sm">
+        <a class="btn btn-primary" href="#page-top">
+            <i class="fa fa-chevron-up"></i>
+        </a>
+    </div>
 </html>
